79437b041020a612943338e8f11d8fb4333b1d7c
Aditya Kumar <aryan68125@gmail.com>
2021-01-08 22:30:08 +0530

    Add files via upload

---
+++ NumberSwapping.py
@@ -0,0 +1,12 @@
+#now this program is written to swap the places of values entered by the user
+a = raw_input("Enter the first number")
+b = raw_input("Enter the second number")
+print("printing numbers before swapping them")
+print("a = "+"%s"%a)
+print("b = "+"%s"%b)
+c=a
+a=b
+b=c
+print("printing numbers after swapping them")
+print("a = "+"%s"%a)
+print("b = "+"%s"%b)
