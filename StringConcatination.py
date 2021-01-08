8f298514bbaa0fcec44bee0d1b0af08c77d47779
Aditya Kumar <aryan68125@gmail.com>
2021-01-08 23:16:15 +0530

    Add files via upload

---
+++ StringConcatination.py
@@ -0,0 +1,5 @@
+#this program will accept the city and your pet name and it will create the name of your band using String concatination
+
+city = input("what is name of the city you were born?\n")
+pet = input("what is the name of your pet?\n")
+print("The name of the band is = "+city+" "+pet)
